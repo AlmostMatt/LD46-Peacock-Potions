42161ad10f94e26fbde23988084a8cf1dd6d6428
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-04-18 12:09:41 -0400

    Rough idea for event framework

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent
+{
+    public void DoEvent()
+    {
+        // pause the simulation
+        GameState.currentStage = GameState.GameStage.GS_EVENT;
+
+        // populate data for the UI to pull from
+        EventState.currentEvent = this;
+        EventState.currentEventText = "Look, an event!";
+        EventState.currentEventOptions = new string[] { "Option 1", "Option 2" };
+    }
+
+    public void PlayerDecision(int choice)
+    {
+        // callback from the UI
+        Debug.Log("Player made choice " + choice);
+    }
+}
---
+++ Assets/Scripts/State/EventState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventState
+{
+    // populate this data and the event UI will pull from it
+    public static GameEvent currentEvent;
+    public static string currentEventText = "DEFAULT TEXT!";
+    public static string[] currentEventOptions = new string[]{ "uninitialized option 1", "uninitialized option 2" };
+
+    // could track history here too, maybe. or somewhere else, idk
+
+}

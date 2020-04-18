782b88638be52e0c9172ca9f97ad758938f2d9f5
Matthew Hyndman <myndman@gmail.com>
2020-04-18 11:22:33 -0400

    Make UI buttons toggle between summary and event views

---
+++ Assets/Scripts/UI/UIControllerSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Manipulates UI objects based on the game state.
+ * Also receives user input from the UI.
+ */
+public class UIControllerSystem : MonoBehaviour
+{
+    public GameObject SummaryView;
+    public GameObject EventView;
+
+    // Use this for initialization
+    void Start()
+    {
+        SummaryView.SetActive(true);
+        EventView.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SummaryScreenOK()
+    {
+        // done with the summary
+        SummaryView.SetActive(false);
+        EventView.SetActive(true);
+    }
+
+    public void MakeDecision()
+    {
+        // Made a choice in response to a decision
+        SummaryView.SetActive(true);
+        EventView.SetActive(false);
+    }
+}

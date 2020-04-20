c23a3d66875f9eab94975ecf84b650c111194eec
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-04-20 13:44:44 -0400

    stub in a class for tracking relationships

---
+++ Assets/Scripts/State/RelationshipState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelationshipState
+{
+    // stores all data related to relationships that can change through events, etc.
+
+    public static float wifeRelationship;
+    public static bool wifeUsedLovePotion; // as an example
+
+    public static float sonRelationship;
+
+    public static float cactusRelationship;
+
+
+}

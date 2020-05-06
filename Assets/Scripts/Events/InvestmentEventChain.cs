78ba5c912988312c622a313e9b0ec37ccf508428
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-05-05 20:41:06 -0400

    Skip and reschedule the investment event if the player doesn't have enough money

--- Assets/Scripts/Events/InvestmentEventChain.cs
+++ Assets/Scripts/Events/InvestmentEventChain.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class InvestmentEventChain
 {
-    
+    private const int COST = 1000;
+
     public static void Init()
     {
         EventState.PushEvent(new InvestmentEventStart(), 3);
     }
 
     private class InvestmentEventStart : GameEvent
-    {
+    {        
+        protected override EventResult EventStart()
+        {
+            if(GameData.singleton.money < COST)
+            {
+                Debug.Log("postponing investment event to next quarter");
+                EventState.PushEvent(new InvestmentEventStart(), GameData.singleton.quarter + 1);
+                return EventResult.SKIP;
+            }
+
+            return EventResult.CONTINUE;
+        }
+
         protected override EventResult OnStage(EventStage currentStage)
         {
             switch(currentStage)
@@ -24,7 +37,7 @@ public class InvestmentEventChain
                     break;
                 case EventStage.DECIDE:
                     EventState.currentEventImage = "faceOther";
-                    EventState.currentEventText = "I've got an investment opportunity. Lend me $1000, and I'll pay you back double.";
+                    EventState.currentEventText = string.Format("I've got an investment opportunity. Lend me {0}, and I'll pay you back double.", Utilities.FormatMoney(COST));
                     EventState.currentEventOptions = new string[]
                     {
                         "Accept the deal",
@@ -34,9 +47,9 @@ public class InvestmentEventChain
                     break;
                 case EventStage.ACCEPT:
                     EventState.currentEventImage = "faceOther";
-                    EventState.currentEventText = "\"You won't regret this!\", he says. He takes your $1000 and leaves.";
+                    EventState.currentEventText = string.Format("\"You won't regret this!\", he says. He takes your {0} and leaves.", Utilities.FormatMoney(COST));
                     EventState.PushEvent(new InvestmentReturnEvent(), GameData.singleton.quarter + 4);
-                    BusinessState.MoneyChangeFromEvent(-1000);
+                    BusinessState.MoneyChangeFromEvent(-COST);
                     EventState.currentEventOptions = EventState.OK_OPTION;
                     return EventResult.DONE;
                 case EventStage.REFUSE:
@@ -52,8 +65,6 @@ public class InvestmentEventChain
 
     private class InvestmentReturnEvent : GameEvent
     {
-
-
         protected override EventResult OnStage(EventStage currentStage)
         {
             switch (currentStage)
@@ -66,9 +77,9 @@ public class InvestmentEventChain
                     break;
                 case EventStage.S2:
                     EventState.currentEventImage = "faceOther";
-                    EventState.currentEventText = "\"Didn't I tell you you wouldn't regret it?\" He hands you $2000, then goes on his way.";
+                    EventState.currentEventText = string.Format("\"Didn't I tell you you wouldn't regret it?\" He hands you {0}, then goes on his way.", Utilities.FormatMoney(COST));
                     EventState.currentEventOptions = EventState.OK_OPTION;
-                    BusinessState.MoneyChangeFromEvent(+2000);
+                    BusinessState.MoneyChangeFromEvent(COST * 2);
                     return EventResult.DONE;
             }
             return EventResult.CONTINUE;
